d73a40203da7bde45ff895b31d93f34f167e097b
Jayendran Arumugam <Jayendran.Arumugam@gds.ey.com>
2019-09-07 23:11:10 +0530

    updated  video search autofill

--- TalentProWebApp/Controllers/VideoController.cs
+++ TalentProWebApp/Controllers/VideoController.cs
@@ -10,6 +10,8 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
 
 namespace TalentProWebApp.Controllers
 {
@@ -20,6 +22,19 @@ namespace TalentProWebApp.Controllers
 
         private readonly int pageSize = Convert.ToInt16(ConfigurationManager.AppSettings["PageSizeForVideo"]);
         private readonly string VideoContainerName = ConfigurationManager.AppSettings["VideoContainerName"].ToString();
+        private static SearchServiceClient _searchClient;
+        private static ISearchIndexClient _indexClient;
+        private static string IndexName = ConfigurationManager.AppSettings["VideoIndexName"];
+
+        private void InitSearch()
+        {
+            string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
+            string apiKey = ConfigurationManager.AppSettings["api-Key"];
+
+            // Create a reference to the NYCJobs index
+            _searchClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
+            _indexClient = _searchClient.Indexes.GetClient(IndexName);
+        }
 
 
         public ActionResult Index()
@@ -81,7 +96,7 @@ namespace TalentProWebApp.Controllers
                     {
                         try
                         {
-                            blockBlob.UploadFromStream(fileStream, accessCondition: AccessCondition.GenerateIfNoneMatchCondition("*"));
+                            blockBlob.UploadFromStream(fileStream, accessCondition: Microsoft.WindowsAzure.Storage.AccessCondition.GenerateIfNoneMatchCondition("*"));
                             LOGGER.Info("video upload to azure blob container completed successfully for " + email+ " with the filename "+file.FileName);
                             message = "File Uploaded Successfully";
                         }
@@ -108,6 +123,55 @@ namespace TalentProWebApp.Controllers
                 ViewBag.Message = "You have not specified a file.";
             }
             return View("UploadVideo");
+        }
+
+        public ActionResult Suggest(bool highlights, bool fuzzy, string term)
+        {
+            InitSearch();
+
+            // Call suggest API and return results
+            SuggestParameters sp = new SuggestParameters()
+            {
+                UseFuzzyMatching = fuzzy,
+                Top = 5
+            };
+
+            if (highlights)
+            {
+                sp.HighlightPreTag = "<b>";
+                sp.HighlightPostTag = "</b>";
+            }
+
+            var suggestResult = _indexClient.Documents.Suggest(term, "suggestor", sp);
+
+            // Convert the suggest query results to a list that can be displayed in the client.
+            List<string> suggestions = suggestResult.Results.Select(x => x.Text).ToList();
+            return new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = suggestions
+            };
+        }
+
+        public ActionResult AutoComplete(string term)
+        {
+            InitSearch();
+            //Call autocomplete API and return results
+            AutocompleteParameters ap = new AutocompleteParameters()
+            {
+                AutocompleteMode = AutocompleteMode.OneTermWithContext,
+                UseFuzzyMatching = false,
+                Top = 5
+            };
+            AutocompleteResult autocompleteResult = _indexClient.Documents.Autocomplete(term, "suggestor", ap);
+
+            // Conver the Suggest results to a list that can be displayed in the client.
+            List<string> autocomplete = autocompleteResult.Results.Select(x => x.Text).ToList();
+            return new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = autocomplete
+            };
         }
 
 
