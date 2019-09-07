a5f9d8c560cb4a830c94aad3279d2aff6b9620c6
Jayendran Arumugam <Jayendran.Arumugam@gds.ey.com>
2019-09-07 22:04:21 +0530

    updated with autofill

--- TalentProWebApp/Controllers/ResumeController.cs
+++ TalentProWebApp/Controllers/ResumeController.cs
@@ -11,6 +11,8 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Azure.Search.Models;
+using Microsoft.Azure.Search;
 
 namespace TalentProWebApp.Controllers
 {
@@ -21,6 +23,19 @@ namespace TalentProWebApp.Controllers
 
         private readonly int pageSize =Convert.ToInt16(ConfigurationManager.AppSettings["PageSize"]);
         private readonly string DocContainerName = ConfigurationManager.AppSettings["DocContainerName"].ToString();
+        private static SearchServiceClient _searchClient;
+        private static ISearchIndexClient _indexClient;
+        private static string IndexName = ConfigurationManager.AppSettings["DocIndexName"];
+
+        private void InitSearch()
+        {
+            string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
+            string apiKey = ConfigurationManager.AppSettings["api-Key"];
+
+            // Create a reference to the NYCJobs index
+            _searchClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
+            _indexClient = _searchClient.Indexes.GetClient(IndexName);
+        }
 
         public ActionResult Index()
         {
@@ -110,6 +125,55 @@ namespace TalentProWebApp.Controllers
 
             return View(items.value);
 
+        }
+
+        public ActionResult Suggest(bool highlights, bool fuzzy, string term)
+        {
+            InitSearch();
+
+            // Call suggest API and return results
+            SuggestParameters sp = new SuggestParameters()
+            {
+                UseFuzzyMatching = fuzzy,
+                Top = 5
+            };
+
+            if (highlights)
+            {
+                sp.HighlightPreTag = "<b>";
+                sp.HighlightPostTag = "</b>";
+            }
+
+            var suggestResult = _indexClient.Documents.Suggest(term, "suggesters", sp);
+
+            // Convert the suggest query results to a list that can be displayed in the client.
+            List<string> suggestions = suggestResult.Results.Select(x => x.Text).ToList();
+            return new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = suggestions
+            };
+        }
+
+        public ActionResult AutoComplete(string term)
+        {
+            InitSearch();
+            //Call autocomplete API and return results
+            AutocompleteParameters ap = new AutocompleteParameters()
+            {
+                AutocompleteMode = AutocompleteMode.OneTermWithContext,
+                UseFuzzyMatching = false,
+                Top = 5
+            };
+            AutocompleteResult autocompleteResult = _indexClient.Documents.Autocomplete(term, "suggesters", ap);
+
+            // Conver the Suggest results to a list that can be displayed in the client.
+            List<string> autocomplete = autocompleteResult.Results.Select(x => x.Text).ToList();
+            return new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = autocomplete
+            };
         }
 
         public static string FindImagewithPersonTag (List<CognitiveImageAnalysis> cognitiveImageAnalyses)
